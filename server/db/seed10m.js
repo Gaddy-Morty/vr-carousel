96844d6ff8931c03358ab38eeb823d028950741c
Gabriel Song <gabsong4@gmail.com>
2020-02-22 09:01:07 -0800

    Clean up seed script

--- server/db/seed10m.js
+++ server/db/seed10m.js
@@ -42,31 +42,7 @@ const createGalleries = (list) => {
 
 const photos = createGalleries(data.Contents);
 
-const addListingPhotos = (start, size, callback) => {
-  const end = start + size;
-  let listingId = start;
-  
-  while (listingId < end) {
-    // pick a random photo gallery to add to a listing
-    const galleries = Object.keys(photos);
-    const gallery = galleries[getRandomInt(0, galleries.length)];
-
-    // pick a random photo as the main photo of the listing
-    const photoIdx = getRandomInt(0, photos[gallery].length);
-
-    // add a listing_id sequentially to all photos in the gallery
-    for (let i = 0; i < photos[gallery].length; i += 1) {
-      const photo = photos[gallery][i];
-      photo.is_main = (i === photoIdx);
-      photo.listing_id = listingId;
-      stream.write(photo);
-    }
-    listingId += 1;
-  }
-  stream.end();
-};
-
-function writePhotosCsv(stream, start, size, callback) {
+const writePhotosCsv = (stream, start, size, callback) => {
   const end = start + size;
   let index = start;
   write();
@@ -78,7 +54,11 @@ function writePhotosCsv(stream, start, size, callback) {
       const photoIdx = getRandomInt(0, photos[gallery].length);
       for (let i = 0; i < photos[gallery].length; i += 1) {
         const photo = photos[gallery][i];
-        photo.is_main = (i === photoIdx);
+        if (i === photoIdx) {
+          photo.is_main = 1;
+        } else {
+          photo.is_main = 0;
+        }
         photo.listing_id = index;
         if (index === end - 1) {
           ok = stream.write(photo, callback);
@@ -99,7 +79,7 @@ const writable = createWriteStream('./server/db/photos.csv');
 const stream = format({ headers: true });
 stream.pipe(writable);
 // uncomment to generate a csv with photos
-writePhotosCsv(stream, Math.pow(10, 7), Math.pow(10, 7), () => { stream.end() });
+// writePhotosCsv(stream, Math.pow(10, 7), Math.pow(10, 7), () => { stream.end() });
 
 const createListingIds = (start, size) => {
   const end = start + size;
