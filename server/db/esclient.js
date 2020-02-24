654ce04c18441e1f21e01bb84d4c70a30790c1d1
Gabriel Song <gabsong4@gmail.com>
2020-02-23 21:06:55 -0800

    Finish elasticsearch seeding script

--- server/db/esclient.js
+++ server/db/esclient.js
@@ -1,45 +1,7 @@
 'use strict'
 
 require('array.prototype.flatmap').shim();
-const data = require('./output.js');
-
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-const captions = [
-  "Warm sunlight throughout",
-  "Everything you need is here",
-  "Rest in tranquil ambiance",
-  "Peek around this place",
-  "Just... perfect",
-  "Enjoy your time here",
-  "Balanced yet 'colorful'",
-  "An interior designer dream",
-  "Natural light permeates",
-  "Ready for you, YASS",
-  "Stay -- you're welcome",
-];
-
-const createGalleries = (list) => {
-  const photos = {};
-  for (let i = 0; i < list.length; i += 1) {
-    const gallery = list[i].Key.split('/')[0];
-    if (photos[gallery] === undefined) {
-      photos[gallery] = [];
-    }
-    // insert object with properties
-    const photo = {};
-    photo.url_path = list[i].Key;
-    photo.caption = captions[getRandomInt(0, captions.length)];
-    photo.space_type = '';
-    photo.order = null;
-    photos[gallery].push(photo);
-  }
-  return photos;
-};
+const s3Export = require('./s3Export.js');
 
 const { Client } = require('@elastic/elasticsearch');
 const client = new Client({
@@ -48,121 +10,131 @@ const client = new Client({
 
 async function createIndex () {
   await client.indices.create({
-    index: 'listing',
-    // the body of the index
-    body: {
-      // define how doc/fields are indexed
-      mappings: {
-        // sub-fields, can be any data type
-        properties: {
-          listings: {
-            properties: {
-              listing_id: { type: 'integer' },
-              photos: {
-                // nested indicates an array of objects
-                type: 'nested',
-                properties: {
-                  url_path: { type: 'text' },
-                  caption: { type: 'text' },
-                  space_type: { type: 'keyword' },
-                  is_main: { type: 'boolean' },
-                }
-              }
-            }
-          }
-        }
+    index: 'photos',
+    body: { // the body of the index
+      properties: {
+        url_path: { type: 'text' },
+        caption: { type: 'text' },
+        space_type: { type: 'keyword' },
+        order: { type: 'integer' },
+        is_main: { type: 'boolean' },
+        listing_id: { type: 'integer' },
       }
     }
   }, { ignore: [400] });
 }
 
-createIndex();
+const createChunks = (dataset, start, size=10000) => {
+  const storage = {};
+  for (let file of dataset) {
+    const key = file.Key.split('/')[0];
+    if (storage[key] === undefined) {
+      storage[key] = [ file.Key ];
+    } else {
+      storage[key].push(file.Key);
+    }
+  }
+
+  const results = [];
+  const samples = [
+    "Warm sunlight throughout",
+    "Everything you need is here",
+    "Rest in tranquil ambiance",
+    "Peek around this place",
+    "Just... perfect",
+    "Enjoy your time here",
+    "Balanced yet 'colorful'",
+    "An interior designer dream",
+    "Natural light permeates",
+    "Ready for you, YASS",
+    "Stay -- you're welcome",
+  ];
+
+  let chunk = 0;
+  do {
+    // pick a random set from storage & a random photo from set
+    const key = Object.keys(storage)[randomInt(0, Object.keys(storage).length)];
+    const main = randomInt(0, storage[key].length);
+
+    // build photo object (i.e. each user-generated photo + caption)
+    for (let i = 0; i < storage[key].length; i += 1) {
+      const photo = {};
+      photo.url_path = storage[key][i];
+      photo.caption = samples[randomInt(0, samples.length)];
+      photo.space_type = '';
+      photo.order = null;
+      (i === main) ? photo.is_main = 1 : photo.is_main = 0;
+      photo.listing_id = start;
+      results.push(photo);
+      chunk += 1;
+    }
+    start += 1;
+  } while (chunk < size);
+
+  return results; // return array of length size=10000 (default)
+
+  function randomInt (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+};
 
 async function run () {
-  // Now, the code to generate the photo object
-  const allPhotos = createGalleries(data.Contents);
-  let dataset = []; // body of the bulk API call
-  const start = Math.pow(10, 7); // starting index, i.e. Math.pow(10, 7)
-  const size = Math.pow(10, 2); // test run
+  const start = Math.pow(10, 7);
+  const size = Math.pow(10, 7);
   const end = start + size;
-  let index = start;
-  let bulkSize = 0; // limit to 10K per call, write ~10K times
-  let bulkCount = 0; // track how many Bulk API calls we make
-  
-  while (index < end) {
-    const galleries = Object.keys(allPhotos);
-    const gallery = galleries[getRandomInt(0, galleries.length)];
-    const photoIdx = getRandomInt(0, allPhotos[gallery].length);
-    for (let i = 0; i < allPhotos[gallery].length; i += 1) {
-      const photo = allPhotos[gallery][i];
-      if (i === photoIdx) {
-        photo.is_main = 1;
-      } else {
-        photo.is_main = 0;
-      }
-      photo.listing_id = index;
-      dataset.push(photo);
-      bulkSize += 1;
+  let currIndex = start;
+  let bulkCalls = 0;
+
+  while (currIndex < end) {
+    const dataset = createChunks(s3Export.Contents, currIndex);
+    const body = dataset.flatMap(doc => [
+      { index: { _index: 'photos' } },
+      doc // each array object
+    ]);
+
+    console.log(body.slice(0,2)); // use to verify progress
+
+    const { body: bulkResponse } = await client.bulk({ refresh: false, body });
+
+    // https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/bulk_examples.html
+    if (bulkResponse.errors) {
+      const erroredDocuments = [];
+      // The items array has the same order of the dataset we just indexed.
+      // The presence of the `error` key indicates that the operation
+      // that we did for the document has failed.
+      bulkResponse.items.forEach((action, i) => {
+        const operation = Object.keys(action)[0];
+        if (action[operation].error) {
+          erroredDocuments.push({
+            // If the status is 429 it means that you can retry the document,
+            // otherwise it's very likely a mapping error, and you should
+            // fix the document before to try it again.
+            status: action[operation].status,
+            error: action[operation].error,
+            operation: body[i * 2],
+            document: body[i * 2 + 1]
+          });
+        }
+      });
+      console.log(erroredDocuments);
     }
 
-    if (bulkSize >= 100) {
-      // dataset is an array of objects
-      // flatMap maps each element and flattens results into a new array
-      // doc is each object with id/text/user/date
-      const body = dataset.flatMap(doc => [
-        {
-          index: {
-            _index: 'listing'
-          }
-        }, doc // each array object
-      ]);
-      // body should be a flattened array
-      
-      // reset closure variables
-      dataset = [];
-      bulkSize = 0;
-
-      // pass object to bulk method
-      // asynchronously store response
-      const { body: bulkResponse } = await client.bulk({ refresh: false, body });
-      await function () { bulkCount += 1; }();
-
-      // error handling logic (from official documentation)
-      if (bulkResponse.errors) {
-        const erroredDocuments = [];
-        // The items array has the same order of the dataset we just indexed.
-        // The presence of the `error` key indicates that the operation
-        // that we did for the document has failed.
-        bulkResponse.items.forEach((action, i) => {
-          const operation = Object.keys(action)[0]
-          if (action[operation].error) {
-            erroredDocuments.push({
-              // If the status is 429 it means that you can retry the document,
-              // otherwise it's very likely a mapping error, and you should
-              // fix the document before to try it again.
-              status: action[operation].status,
-              error: action[operation].error,
-              operation: body[i * 2],
-              document: body[i * 2 + 1]
-            });
-          }
-        });
-        console.log(erroredDocuments);
-      }
-    }
-
-    // Add the photo set for the next listing
-    index += 1;
+    // wrapping code within `await new Promise` guarantees that this code block
+    // executes only after the previous promise (i.e. client.bulk) is resolved
+    await new Promise((resolve) => {
+      bulkCalls += 1;
+      currIndex += 1000; // each chunk contains 1000 listings, 10 photos each
+      console.log('ListingId:', currIndex, 'Bulk API:', bulkCalls);
+      resolve();
+    });
   }
-  const { body: count } = await client.count({ index: 'listing' });
-  // logs out the total index count
-  console.log('Elasticsearch index count:', count, '\n', 'Bulk API calls:', bulkCount);
 }
 
-run().catch(console.log);
-
-// (async function() {
-//   for await (let asyncReturn of run()) {
-//     asyncReturn.catch(console.log)
-//   }
-// })();
+createIndex()
+  .then(() => console.log('ES index created, adding data next!'))
+  .then(() => run())
+  .then(() => client.count({ index: 'photos' }))
+  .then(({ body: count }) => console.log('ES index count:', count))
+  .catch((err) => console.log(err));
