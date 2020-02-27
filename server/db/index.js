91b2a405681c0ddf54ed1ecd327a630c48bc9b7a
Gabriel Song <gabsong4@gmail.com>
2020-02-26 20:57:35 -0800

    Remove unnecessary code

--- server/db/index.js
+++ server/db/index.js
@@ -1,36 +1,43 @@
-const mongoose = require('mongoose');
+const { Client } = require('@elastic/elasticsearch');
+const hostname = process.env.ES_HOST || 'localhost';
+const esclient = new Client({
+  node: `http://${hostname}:9200`,
+  log: 'error'
+});
+
+module.exports.esclient = esclient;
+
+// ###### MARIADB ######
+// const mariadb = require('mariadb');
+// const pool = mariadb.createPool({
+//      host: '127.0.0.1', 
+//      user:'gabrielsong',
+//      database: 'vacayhome',
+//      password: ''
+// });
+
+// async function asyncFunction() {
+//   let conn;
+//   try {
+//     conn = await pool.getConnection();
+//     console.log(`connected ! connection id is ${conn.threadId}`);
+//   } catch (err) {
+// 	  throw err;
+//   } finally {
+// 	  if (conn) return conn.end();
+//   }
+// }
+
+// module.exports.pool = pool;
+
+// ###### MONGODB ######
+// const mongoose = require('mongoose');
 // const mongoUrl = 'mongodb://database/gallery'; // for docker-compose
 
-const { Client } = require('@elastic/elasticsearch');
-const host = process.env.ES_HOST || 'localhost';
-const esclient = new Client({ host: { host, 9200 } });
+// const GallerySchema = mongoose.Schema({
+//   listing_id: { type: Number, required: true, unique: true },
+//   listing_title: { type: String },
+//   listing_images: [{ id: Number, url: String, caption: String }],
+// });
 
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-     host: '127.0.0.1', 
-     user:'gabrielsong',
-     database: 'vacayhome',
-     password: ''
-});
-
-async function asyncFunction() {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    console.log(`connected ! connection id is ${conn.threadId}`);
-  } catch (err) {
-	  throw err;
-  } finally {
-	  if (conn) return conn.end();
-  }
-}
-
-const GallerySchema = mongoose.Schema({
-  listing_id: { type: Number, required: true, unique: true },
-  listing_title: { type: String },
-  listing_images: [{ id: Number, url: String, caption: String }],
-});
-
-module.exports.GalleryModel = mongoose.model('Gallery', GallerySchema);
-module.exports.esclient = esclient;
-module.exports.pool = pool;
+// module.exports.GalleryModel = mongoose.model('Gallery', GallerySchema);
--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
 // require('elastic-apm-node').start({ serviceName: 'vr-carousel' });
 require('newrelic');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const express = require('express');
-const path = require('path');
 const app = express();
-const port = 3000;
-const controllers = require('./controllers');
-const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
 const compression = require('compression');
-// const seedDb = require('./db/seed.js'); // seed DB every time server runs
+const port = 3000;
+const controllers = require('./controllers/index.js');
 const router = require('./routes.js');
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(compression());
 
